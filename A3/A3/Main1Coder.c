09d31564c8f2def663d7018b404614ceebf2ecda
Suleiman Amin <rigaax87@gmail.com>
2026-03-19 13:15:34 -0400

    Add A3 Scanner implementation (Rigaax language)

---
+++ A3/A3/Main1Coder.c
@@ -0,0 +1,143 @@
+/*
+************************************************************
+* COMPILERS COURSE - Algonquin College
+* Code version: WINTER, 2026
+* Author: Suleiman Amin - 041 130 274 / Blessy Joy - 041 134 4029
+* Student Name: Suleiman Amin
+* Student ID: 041130274
+* Professors: Paulo Sousa
+************************************************************
+#
+# ECHO "=---------------------------------------="
+# ECHO "|  COMPILERS - ALGONQUIN COLLEGE (F25)  |"
+# ECHO "=---------------------------------------="
+# ECHO "    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@    ”
+# ECHO "    @@                             @@    ”
+# ECHO "    @@           %&@@@@@@@@@@@     @@    ”
+# ECHO "    @@       @%% (@@@@@@@@@  @     @@    ”
+# ECHO "    @@      @& @   @ @       @     @@    ”
+# ECHO "    @@     @ @ %  / /   @@@@@@     @@    ”
+# ECHO "    @@      & @ @  @@              @@    ”
+# ECHO "    @@       @/ @*@ @ @   @        @@    ”
+# ECHO "    @@           @@@@  @@ @ @      @@    ”
+# ECHO "    @@            /@@    @@@ @     @@    ”
+# ECHO "    @@     @      / /     @@ @     @@    ”
+# ECHO "    @@     @ @@   /@/   @@@ @      @@    ”
+# ECHO "    @@     @@@@@@@@@@@@@@@         @@    ”
+# ECHO "    @@                             @@    ”
+# ECHO "    @@         R I G A A X         @@    ”
+# ECHO "    @@                             @@    ”
+# ECHO "    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@    ”
+# ECHO "                                         "
+# ECHO "[CODER SCRIPT ..........................]"
+# ECHO "                                         "
+*/
+
+/*
+************************************************************
+* File name: MainCoder.c
+* Compiler: MS Visual Studio 2022
+* Course: CST 8152 – Compilers, Lab Section: [011, 012]
+* Assignment: A12, A22, A32.
+* Date: Sep 01 2025
+* Professor: Paulo Sousa
+* Purpose: This file is the main code for Buffer/Reader (A12)
+* Function list: (...).
+*************************************************************/
+
+/*
+ *.............................................................................
+ * ADVICE 1:
+ * Please check the "TODO" labels to develop your activity.
+ *
+ * ADVICE 2: Preprocessor directives
+ * The #define _CRT_SECURE_NO_WARNINGS should be used in MS Visual Studio projects
+ * to suppress the warnings about using "unsafe" functions like fopen()
+ * and standard sting library functions defined in string.h.
+ * The define directive does not have any effect on other compiler projects 
+ * (Gcc, VSCode, Codeblocks, etc.).
+ *.............................................................................
+ */
+
+#define _CRT_SECURE_NO_WARNINGS 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <ctype.h>
+
+#ifndef COMPILERS_H_
+#include "Compilers.h"
+#endif
+
+#ifndef CODER_H_
+#include "Step1Coder.h"
+#endif
+
+ /* Check for ANSI C compliancy */
+#define ANSI_C 0
+#if defined(__STDC__)
+#undef ANSI_C
+#define ANSI_C 1
+#endif
+
+/*
+ * TODO .......................................................................
+ * Basically, change all datatypes to your language definitions
+ * (see "Compilers.h")
+ */
+
+/*
+ * -------------------------------------------------------------
+ *  Function declarations
+ * -------------------------------------------------------------
+ */
+
+/*
+************************************************************
+* Coder starting method
+* Params:
+*	- Mode: Cypher/decypher
+*	- Input: Filename
+*   - Key: Encrypt word
+*	- Output: Filename
+************************************************************
+*/
+
+// Main function to handle command-line arguments
+rigaax_intg main1Coder(rigaax_intg argc, rigaax_strg* argv) {
+	if (argc < 5) {
+		printf("Usage: %s [cypher=1|decypher=0] <input_file> <output_file>\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+	rigaax_strg operation = "";
+	rigaax_strg inputFileName = "";
+	rigaax_strg key = STR_LANGNAME;
+	rigaax_strg outputFileName = "";
+	if (argc > 4) {
+		operation = argv[2];
+		inputFileName = argv[3];
+		outputFileName = argv[4];
+		// Call the appropriate function to file
+		if (atoi(operation) == CYPHER)
+			cypher(inputFileName, outputFileName, key);
+		else if (atoi(operation) == DECYPHER)
+			decypher(inputFileName, outputFileName, key);
+		else {
+			errorPrint("%s%s%s", "Error: Unknown operation ", operation, ". Use 'cypher' or 'decypher'.\n");
+			return EXIT_FAILURE;
+		}
+		printf("Operation '%s' completed successfully.\n", operation);
+		// Call the other operation in memory
+		rigaax_intg size = getSizeOfFile(outputFileName);
+		rigaax_strg output;
+		if (atoi(operation) == CYPHER)
+			output = vigenereMem(outputFileName, key, DECYPHER);
+		else 
+			output = vigenereMem(outputFileName, key, CYPHER);
+		printf("OUTPUT:\n%s\n", output);
+	}
+	return EXIT_SUCCESS;
+}
+
